dd5e4dadd07dd1bc5ffd85aa0c6f19a921108d66
rakeshmahato-png <rakesh.mahato@rungta.org>
2025-12-27 10:08:53 +0530

    first commit

---
+++ practicals/index.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("\n RAKESH MAHATO \nERP-RU-25-11117\n");
+    printf("Hello ! I am Rakesh Mahato\n");
+    printf("I am 19 Years old");
+
+    return 0;
+}
